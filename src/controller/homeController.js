92855a768910c3acee9ae5b846ef068357c7eaa3
Khải Lê <thanhkhai1234567890@gmail.com>
2024-08-09 16:05:53 +0700

    use bootstrap

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,10 @@
 const handleAccessWeb = (req, res) => {
   return res.render("home.ejs");
 };
+const handleUser = (req, res) => {
+  return res.render("user.ejs");
+};
 module.exports = {
   handleAccessWeb,
+  handleUser,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", homeController.handleAccessWeb);
+  router.get("/user", homeController.handleUser);
   return app.use("/", router);
 };
 
